82ec2385aa252003973734900521f402997e3eea
Tanmay Mathur <tanmaymathur89@gmail.com>
2012-08-26 16:43:46 -0400

    Pre-final

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -11,10 +11,19 @@ class CatPictureApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+private:
+	float change;
+	float change1;
+	float change2;
+	int i;
 };
 
 void CatPictureApp::setup()
 {
+	i=0;
+	change=0.0f;
+	change1=0.5f;
+	change2=1.0f;
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -23,12 +32,16 @@ void CatPictureApp::mouseDown( MouseEvent event )
 
 void CatPictureApp::update()
 {
+	i++;
+	if(change<=1)change=change+.01f; else {change=change/(2*i);}
+if(change1<=1)change1=change1+.01f;else {change1=change1/(2*i);}
+if(change2>=0)change2=change2-.01f;else {change2=change2/(2*i);}
 }
 
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 1, 1, 0 ) ); 
+	gl::clear( Color( change, change1,change2) ); 
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
